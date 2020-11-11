e2535578d104584bdf368acf5b6465296ab318ad
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-11-11 13:45:02 +0000

    create basic flask app file structure

---
+++ calendar/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template, url_for, flash, redirect
+from calendar import app
+# from calendar.models import User, Post
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/login")
+def about():
+    return render_template('about.html')
